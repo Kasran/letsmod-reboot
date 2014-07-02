f72063a0f3be20fde0ed3551ccb5db05247ca5d1
Kasran Fox <martin.riatsu.kasran@gmail.com>
2014-07-01 23:09:01 -0400

    Blargh

--- src/main/java/com/kasran/letsmodreboot/config/ConfigHandler.java
+++ src/main/java/com/kasran/letsmodreboot/config/ConfigHandler.java
@@ -7,10 +7,13 @@ import net.minecraftforge.common.config.Configuration;
 public class ConfigHandler {
 	public static void init(File configFile) {
 		Configuration cfg = new Configuration(configFile);
+		
+		boolean configValue;
 		try {
 			cfg.load();
 			
 			// read stuff in from cfg
+			configValue = cfg.get(Configuration.CATEGORY_GENERAL, "configValue", true, "blah comment").getBoolean(true);
 		}
 		catch(Exception e) {
 			// log exception
@@ -18,5 +21,7 @@ public class ConfigHandler {
 		finally {
 			cfg.save();
 		}
+		
+		System.out.println(configValue);
 	}
 }
